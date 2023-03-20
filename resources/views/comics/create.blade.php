745a22417019ad94b347bd51123236927d6bad6e
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-03-20 18:04:40 +0100

    Laravel dc comics

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -15,17 +15,30 @@
         <div class="row">
             <div class="col">
                 <form action="{{ route('comics.store') }}" method="POST">
+                    @csrf
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Titolo fumetto</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <label for="title" class="form-label">Titolo fumetto</label>
+                        <input type="text" class="form-control" name="title" id="title" aria-describedby="emailHelp">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Serie</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1">
+                        <label for="description" class="form-label">Descrizione</label>
+                        <input type="text" class="form-control" name="description" id="description">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Prezzo</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1">
+                        <label for="price" class="form-label">Prezzo</label>
+                        <input type="text" class="form-control" name="price" id="price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="series" class="form-label">Serie</label>
+                        <input type="text" class="form-control" name="series" id="series">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">Data di uscita</label>
+                        <input type="text" class="form-control" name="sale_date" id="sale_date">
+                    </div>
+                    <div class="mb-3">
+                        <label for="type" class="form-label">Tipologia</label>
+                        <input type="text" class="form-control" name="type" id="type">
                     </div>
                     <button type="submit" class="btn btn-primary">Aggiungi</button>
                 </form>
